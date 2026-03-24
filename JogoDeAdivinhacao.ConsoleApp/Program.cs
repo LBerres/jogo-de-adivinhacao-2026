f56773406cbda9303d49b3d436a12c752f9ff01a
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-03-24 20:58:28 -0300

    Adicionada as Funcionalidades de Validação e de Pontuação.

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -16,17 +16,23 @@ O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dica
 v2
 
 1. Implemente a funcionalidade de Dificuldade e Tentativas limitadas - OK
-
     O jogador tem um número limitado de tentativas para adivinhar o número.
         Fácil (intervalo 1 a 20): ≈ 10 tentativas.
         Médio (intervalo 1 a 50): ≈ 5 tentativas.
         Difícil (intervalo 1 a 100): ≈ 3 tentativas.
+
+2. Implemente uma funcionalidade de Validação de Números Repetidos - OK
+    O jogador deve ser informado caso o número que está tentando adivinhar já tenha sido informado anteriormente na mesma rodada.
+
+3. Implemente uma funcionalidade de Pontuação, onde: - OK
+    O jogador começa com uma pontuação máxima, por exemplo, 1000 pontos.
+    A pontuação é calculada com base na proximidade do palpite em relação ao número secreto.
+    A cada tentativa errada, o jogador perde pontos de acordo com a distância do número secreto:
+    Se a diferença entre o número secreto e o palpite for de 10 ou mais, o jogador perde 100 pontos.
+    Se a diferença for entre 5 e 9, o jogador perde 50 pontos.
+    Se a diferença for entre 1 e 4, o jogador perde 20 pontos.
+    Quando o jogador acerta o número, sua pontuação final é registrada.
 */
-
-
-
-// 1 - 20 (numero Mínimo, numero Máximo (exclusivo))
-
 bool jogoDeveContinuar = true;
 
 while (jogoDeveContinuar)
@@ -71,6 +77,9 @@ while (jogoDeveContinuar)
             continue; // Volta para o Início do Loop
 
     }
+    int pontuacao = 1000; // Variável para Armazenar a Pontuação do Jogador
+    int[] numerosDigitados = new int[tentativasMaximas]; // Array para Armazenar os Números Digitados
+    int contadorNumerosDigitados = 0; // Contador para Controlar a Quantidade de Números Digitados
 
     // Enquanto a Tentativa Atual For Menor que a Quantidade Máxima de Tentativas, o Jogo Continua
     for (int tentativaAtual = 1; tentativaAtual <= tentativasMaximas; tentativaAtual++)
@@ -80,10 +89,50 @@ while (jogoDeveContinuar)
         Console.WriteLine("Jogo de Adivinhação");
         Console.WriteLine("----------------------------------------------");
         Console.WriteLine($"Tentativa {tentativaAtual} de {tentativasMaximas}");
+        Console.WriteLine("Pontuação Atual: " + pontuacao);
         Console.WriteLine("----------------------------------------------");
+
         Console.Write("Digite um Número: ");
         string strNumeroDigitado = Console.ReadLine();
         int numeroDigitado = Convert.ToInt32(strNumeroDigitado);
+
+        //Comparar o Número Digitado com os Números Anteriores
+        bool numeroRepetido = false;
+
+        for (int indiceatual = 0; indiceatual < numerosDigitados.Length; indiceatual++)
+        {
+            if (numerosDigitados[indiceatual] == numeroDigitado)
+            {
+                numeroRepetido = true;
+                break; // Sair do Loop se Encontrar um Número Repetido
+            }
+        }
+
+        if (numeroRepetido == true)
+        {
+            Console.WriteLine("Você Já Digitou Esse Número Anteriormente! Tente um Número Diferente.");
+            Console.WriteLine("----------------------------------------------");
+            Console.Write("Digite Enter Para Continuar...");
+            Console.ReadLine();
+
+            tentativaAtual--; // Decrementar a Tentativa Atual para Não Contar a Tentativa Repetida
+            continue;
+        }
+
+        //Guardar o Número na Memória
+        if (contadorNumerosDigitados < numerosDigitados.Length)
+        {
+            numerosDigitados[contadorNumerosDigitados] = numeroDigitado;
+            contadorNumerosDigitados++;
+        }
+        else
+        {
+            numerosDigitados = new int[tentativasMaximas];
+            contadorNumerosDigitados = 0;
+
+            numerosDigitados[contadorNumerosDigitados] = numeroDigitado;
+            contadorNumerosDigitados++;
+        }
 
         if (numeroAleatorio == numeroDigitado)
         {
@@ -99,10 +148,27 @@ while (jogoDeveContinuar)
             Console.WriteLine("Dica: O Número Digitado é Menor do que o Número Secreto.");
         }
 
+        int diferencaNumerica = Math.Abs(numeroAleatorio - numeroDigitado);
+
+        if (diferencaNumerica >= 10)
+        {
+            pontuacao -= 100;
+        }
+        else if (diferencaNumerica >= 5)
+        {
+            pontuacao -= 50;
+        }
+        else
+        {
+            pontuacao -= 20;
+        }
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine("Pontuação Atual: " + pontuacao);
         Console.WriteLine("----------------------------------------------");
         Console.Write("Digite Enter Para Continuar...");
         Console.ReadLine();
     }
+
 
     Console.WriteLine();
     Console.WriteLine("Deseja Jogar Novamente? (S/N)");
